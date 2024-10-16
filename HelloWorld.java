4f98fa40752b9c611bfc5d55d7a9a547ac601a6e
Subash-Chandran0 <subashchandran.c0@gmail.com>
2024-10-16 08:11:06 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+import com.sun.jdi.Value;
+
+import java.security.cert.CertStoreSpi;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" i  am programmaer");
+        System.out.print("Hello world");
+        System.out.print("i love java ");
+    }
+}
+//variables
+
+
+  //variable name : A label for a memory location
+//Value: The something that would be stored on a variable
+//Storage:A place where data can be stored
+//Declaration: Announcing a variable(usually) at the beginning of a program
+//Naming convention: A set of rules about the names of variables
+//Assignment:Giving (setting) a variable a value
